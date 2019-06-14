28b673df5d14f02bc61cb8efc9858b9a116afdab
Iskra B <ibatistapoblete@gmail.com>
2019-06-14 04:30:01 +0000

    first commit

---
+++ lib/dog_breed_selector.rb
@@ -0,0 +1,6 @@
+require "dog_breed_selector/version"
+
+module DogBreedSelector
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dog_breed_selector/version.rb
@@ -0,0 +1,3 @@
+module DogBreedSelector
+  VERSION = "0.1.0"
+end
